0d6f8c557eea8b23c1723a1c38fef3b1d362ffa0
Salma333-ss <129120991+Salma333-ss@users.noreply.github.com>
2023-04-03 09:07:40 -0700

    Add files via upload

---
+++ S1.py
@@ -0,0 +1,31 @@
+# SALMA SANUSI ALI AHMED
+# 10985870
+# FPEN
+import qrcode
+import PySimpleGUI as sg 
+
+qrc_image = [sg.Image('',key='-QRCODE-',size=(350,350),background_color='black')]
+
+layout=[
+    [sg.Input('',key='-URL-',justification='center')],
+    [sg.Button('Create',key='-submit-',expand_x= True,button_color='pink')],
+    [sg.Column([qrc_image], justification= 'center')],
+]
+
+wind =sg.Window('QRCode Generator APP',layout)
+
+while True:
+    event,values = wind.read()
+    if event==sg.WIN_CLOSED:
+        break
+    elif event=='-submit-':
+        url= values['-URL-']
+        if url:
+            image= qrcode.make(url)
+            image.save('qr.png')
+            wind['-QRCODE-'].update('qr.png')
+wind.close()
+
+
+
+
